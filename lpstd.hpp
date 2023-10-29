179f7b4107bacbd867c96b4d5d05dc76ffba41dc
lucaspalomodevelop <lucas.palomo@t-online.de>
2023-10-29 21:19:37 +0100

    some changes && add command class

--- lpstd.hpp
+++ lpstd.hpp
@@ -2,10 +2,15 @@
 #ifndef __LPSTD_HPP__
 #define __LPSTD_HPP__
 
+#ifdef _WIN32
+#include <cstdio>
+#include <windows.h>
+#endif
 
 #include "src/patterns/singleton.hpp"
 #include "src/testing/testing.hpp"
 // #include "src/faker/faker.hpp"
 #include "src/exceptions/exceptions.hpp"
+#include "src/classes/command.hpp"
 
 #endif // __LPSTD_HPP__
